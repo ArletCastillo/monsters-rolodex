c2846eb72af3006215c6037f17a9e86f1fc4f27b
ArletCastillo <arletmarcel24@gmail.com>
2023-02-20 20:22:45 -0400

    getting data from a webpage

--- src/App.js
+++ src/App.js
@@ -8,25 +8,21 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '63f014a36e8e69a3e6943927'
-        },
-        {
-          name: 'Frank',
-          id: '63f014b6f7e4fcce192d9ddf' 
-        },
-        {
-          name: 'Jacky',
-          id: '63f014c8f9283ee223e48dd4'
-        },
-        {
-          name: 'Brad',
-          id: '63f014d9e4364855f4d3c8e5'
-        }
-      ]
+      monsters: []
     }
+  }
+
+  // lifecycle method
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
+    }
+    ))
   }
 
   render() {
