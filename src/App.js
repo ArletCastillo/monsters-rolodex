03842b950b95fa1d31005f2f126ab24f5933cc82
ArletCastillo <arletmarcel24@gmail.com>
2023-03-26 19:03:02 -0400

    optimizations

--- src/App.js
+++ src/App.js
@@ -25,20 +25,27 @@ class App extends Component {
     ))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase().trim();
+    this.setState(() => {
+      return { searchField }
+    });
+   }
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    // Destructuring
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters'
-               onChange={(event) => {
-                const searchField = event.target.value.toLocaleLowerCase().trim();
-                this.setState(() => {
-                  return { searchField }
-                });
-               }} />
+               onChange={ onSearchChange } />
         {filteredMonsters.map((monster) => {
           return <div key={monster.id}><h1>{monster.name}</h1></div>
         })}
