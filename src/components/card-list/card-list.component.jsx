a338afaadb39e93d3fd4d13428e164bd0b01731a
ArletCastillo <arletmarcel24@gmail.com>
2023-03-27 20:50:12 -0400

    moved filtered monsters list to component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,8 +2,14 @@ import { Component } from "react";
 
 class CardList extends Component {
     render() {
+        const { monsters } = this.props;
+
         return (
-            <div>Hello I'm the CardList component</div>
+            <div>
+                {monsters.map((monster) => {
+                    return <div key={monster.id}><h1>{monster.name}</h1></div>
+                })}
+            </div>
         );
     }
 }
