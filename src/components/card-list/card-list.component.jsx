8a856995edc10f4440aaaf3af4132bcfe7be6776
ArletCastillo <arletmarcel24@gmail.com>
2023-03-29 20:11:33 -0400

    displaying more monster properties

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,7 +7,14 @@ class CardList extends Component {
         return (
             <div>
                 {monsters.map((monster) => {
-                    return <div key={monster.id}><h1>{monster.name}</h1></div>
+                    const {name, email, id} = monster;
+                    return(
+                        <div className="card-container" key={id}>
+                            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )
                 })}
             </div>
         );
