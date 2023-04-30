128aad18e7e6cfa9009400e8967d2f730bafd6cd
lebron.a <lebron.a@infox.de>
2023-04-30 15:29:19 +0200

    converted cardList into a functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,22 +1,16 @@
-import { Component } from "react";
 import CardContent from "../card-content/card-content.component";
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props;
-
-        return (
-            <div className="card-list">
-                {monsters.map((monster) => {
-                    const {name, email, id} = monster;
-                    return(
-                        <CardContent id={id} name={name} email={email} />
-                    )
-                })}
-            </div>
-        );
-    }
-}
+// destructuring as the parameter of the functional component / implicit return
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map((monster) => {
+            const {name, email, id} = monster;
+            return(
+                <CardContent id={id} name={name} email={email} />
+            )
+        })}
+    </div>
+)
 
 export default CardList;
