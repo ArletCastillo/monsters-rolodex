3727f4b748ff520765425f94482118388f016d11
lebron.a <lebron.a@infox.de>
2023-04-30 15:11:55 +0200

    added useeffect hook to app.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ const App = () => {
   // array destructuring
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  // returns an empty array because this function is not anymore, just when the component mounts
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users))}, 
+  []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase().trim();
     setSearchField(searchFieldString);
   }
-
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLocaleLowerCase().includes(searchField);
-  });
 
   return(
     <div className="App">
@@ -27,56 +38,5 @@ const App = () => {
   );
 
 }
-
-// class App extends Component {
-
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: ''
-//     }
-//   }
-
-//   // lifecycle method
-//   componentDidMount() {
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//     .then((response) => response.json())
-//     .then((users) => this.setState(() => {
-//       return {monsters: users}
-//     },
-//     () => {
-//       //console.log(this.state);
-//     }
-//     ))
-//   }
-
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLocaleLowerCase().trim();
-//     this.setState(() => {
-//       return { searchField }
-//     });
-//    }
-
-//   render() {
-
-//     // Destructuring
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-
-//     return (
-//       <div className="App">
-//         <h2 className='app-title'>Monsters Rolodex</h2>
-//         <SearchBox onChangeHandler={ onSearchChange } placeholder='search monsters' className='monsters-search-box' />
-//         <CardList monsters={ filteredMonsters } />
-//       </div>
-//     );  
-//   }
-// }
 
 export default App;
